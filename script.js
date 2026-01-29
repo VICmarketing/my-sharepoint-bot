97ddf7232ad434296645d8bdb8d1d39e90978f8c
VICmarketing <adwordvic@gmail.com>
2026-01-29 15:03:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('script.js loaded ✅');
 // ใส่ URL ของ n8n Webhook
-const N8N_WEBHOOK_URL = 'http://vic-n8n.store:3001/webhook-test/d102148d-d88b-4754-aecc-39054c4dde00';
+const N8N_WEBHOOK_URL = 'http://localhost:3001/webhook-test/98008d72-fd8b-4694-8d3f-30c1dcc0a130';
 
 const chatHistory = document.getElementById('chat-history');
 const messageInput = document.getElementById('message-input');
@@ -47,4 +47,5 @@ function addMessageToHistory(message, className) {
   chatHistory.appendChild(messageElement);
   chatHistory.scrollTop = chatHistory.scrollHeight;
 }
+
 
